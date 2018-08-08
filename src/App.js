a25fc5421cff9d71727bb6185325a8001771b4c0
acpatel <acpatel@andrew.cmu.edu>
2018-08-08 17:17:57 -0400

    WORKING TITLE PROFILEPIC LINKS

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
-import Profile from './Components/Profile'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { fab } from '@fortawesome/free-brands-svg-icons'
+
+import Profile from './Components/Profile'
+import Title from './Components/Title'
 
 import './App.scss'
 
@@ -12,6 +14,7 @@ class App extends Component {
   render() {
     return (
       <div className="site-container">
+        <Title />
         <Profile />
       </div>
     );
--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -7,248 +7,153 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import imgPath from './Images/profilepic.jpg'
 import './Profile.scss'
 
-class ProfileAnimation extends React.Component {
-  constructor(props) {
-    super(props);
-    this.picRef = React.createRef()
-  }
+const links = [
+  'facebook',
+  'twitter',
+  'instagram',
+  'spotify',
+  'youtube',
+  'soundcloud'
+]
 
-  componentDidUpdate() {
-    const status = this.props.status
-    this.animeTimeline = anime.timeline();
-
-    console.log(this.picRef.current)
-
-    if (status === 'entering') {
-      this.animeTimeline.add({
-        easing: 'easeInOutQuart',
-        loop: false,
-        targets: this.picRef.current,
-        scale: 1.5,
-        duration: 500,
-        complete: this.props.registerCompletedAnimation
-      });
-    } else if (status === 'exiting') {
-      this.animeTimeline.add({
-        easing: 'easeInOutQuart',
-        loop: false,
-        targets: this.picRef.current,
-        scale: 1,
-        duration: 500,
-        complete: this.props.registerCompletedAnimation
-      });
-    }
-  }
-
-  render() {
-    return (
-      <div>
-        <img src={imgPath} className="profile-pic" ref={this.picRef} alt="Profile Pic" onClick={this.props.handleToggle} />
-      </div>
-    );
-  }
+const hyperlinks = {
+  facebook: 'https://www.facebook.com/maxpatelmusic/',
+  twitter: 'https://twitter.com/MaxPatel_Music',
+  instagram: 'https://www.instagram.com/maxpatelmusic/',
+  spotify: 'https://open.spotify.com/artist/3HcJd4UuaOq2pVginUi5MY',
+  youtube: 'https://www.youtube.com/channel/UCPtjLE0quEXBH3GH7Kk9m_g',
+  soundcloud: 'https://soundcloud.com/maximilian-patel'
 }
 
-// const translateX = {
-//   facebook: -120,
-//   twitter: -80,
-//   instagram: -26,
-//   spotify: 26,
-//   youtube: 80,
-//   soundcloud: 120
-// }
+const translateX = {
+  facebook: -120,
+  twitter: -80,
+  instagram: -30,
+  spotify: 30,
+  youtube: 80,
+  soundcloud: 120
+}
 
-// const translateY = {
-//   facebook: -150,
-//   twitter: -200,
-//   instagram: -225,
-//   spotify: -225,
-//   youtube: -200,
-//   soundcloud: -150
-// }
+const translateY = {
+  facebook: -50,
+  twitter: -95,
+  instagram: -120,
+  spotify: -120,
+  youtube: -95,
+  soundcloud: -50
+}
 
-// const delay = {
-//   facebook: 200,
-//   twitter: 100,
-//   instagram: 25,
-//   spotify: 25,
-//   youtube: 100,
-//   soundcloud: 200
-// }
-
-// const hyperlinks = {
-//   facebook: 'https://www.facebook.com/maxpatelmusic/',
-//   twitter: 'https://twitter.com/MaxPatel_Music',
-//   instagram: 'https://www.instagram.com/maxpatelmusic/',
-//   spotify: 'https://open.spotify.com/artist/3HcJd4UuaOq2pVginUi5MY',
-//   youtube: 'https://www.youtube.com/channel/UCPtjLE0quEXBH3GH7Kk9m_g',
-//   soundcloud: 'https://soundcloud.com/maximilian-patel'
-// }
-
-// const links = [
-//   'facebook',
-//   'twitter',
-//   'instagram',
-//   'spotify',
-//   'youtube',
-//   'soundcloud'
-// ]
-
-// // END PARAMETERS //
-
-// class Links extends React.Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.facebookRef = React.createRef()
-//     this.twitterRef = React.createRef()
-//     this.instagramRef = React.createRef()
-//     this.spotifyRef = React.createRef()
-//     this.youtubeRef = React.createRef()
-//     this.soundcloudRef = React.createRef()
-
-//     this.componentDidUpdate = this.componentDidUpdate.bind(this)
-//     this.generateLinkElement = this.generateLinkElement.bind(this)
-//   }
-
-//   generateLinkElement = (link) =>
-//     <div ref={this[`${link}Ref`]} key={link}>
-//       <a className="icon-outer" href={hyperlinks[link]}>
-//         <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40">
-//           <FontAwesomeIcon icon={['fab', link]} />
-//         </svg>
-//       </a>
-//     </div>
-
-//   componentDidUpdate() {
-//     const status = this.props.status
-
-//     if (status === 'entering') {
-//       for (let i = 0; i < links.length; i++) {
-//         const link = links[i]
-//         this[`${link}AnimeRef`] = anime({
-//           easing: 'easeInOutQuart',
-//           loop: false,
-//           targets: this[`${link}Ref`].current,
-//           translateX: translateX[link],
-//           translateY: translateY[link],
-//           delay: delay[link],
-//           scale: 1.2,
-//           opacity: 1,
-//           duration: 500,
-//           complete: () => { if (i === links.length - 1) this.props.registerCompletedAnimation }
-//         });
-//       }
-//     } else if (status === 'exiting') {
-//       for (let i = 0; i < links.length; i++) {
-//         const link = links[i]
-//         this[`${link}AnimeRef`] = anime({
-//           easing: 'easeInOutQuart',
-//           loop: false,
-//           targets: this[`${link}Ref`].current,
-//           translateX: 0,
-//           translateY: 0,
-//           delay: delay[link],
-//           scale: 1,
-//           opacity: 0,
-//           duration: 500,
-//           complete: () => { if (i === links.length - 1) this.props.registerCompletedAnimation }
-//         });
-//       }
-//     }
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         {links.map(link => this.generateLinkElement(link))}
-//       </div>
-//     );
-//   }
-// }
-
-// class Music extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.ref = React.createRef()
-//     this.componentDidUpdate = this.componentDidUpdate.bind(this)
-//   }
-
-//   componentDidUpdate() {
-//     const status = this.props.status
-
-//     if (status === 'entering') {
-//       for (let i = 0; i < links.length; i++) {
-//         const link = links[i]
-//         this.animeRef = anime({
-//           easing: 'easeInOutQuart',
-//           loop: false,
-//           targets: this.ref.current,
-//           translateX: 500,
-//           delay: 300,
-//           scale: 2,
-//           opacity: 1,
-//           duration: 700,
-//           complete: this.props.registerCompletedAnimation
-//         });
-//       }
-//     } else if (status === 'exiting') {
-//       for (let i = 0; i < links.length; i++) {
-//         const link = links[i]
-//         this.animeRef = anime({
-//           easing: 'easeInOutQuart',
-//           loop: false,
-//           targets: this.ref.current,
-//           translateX: 0,
-//           delay: 300,
-//           scale: 1,
-//           opacity: 0,
-//           duration: 700,
-//           complete: this.props.registerCompletedAnimation
-//         });
-//       }
-//     }
-//   }
-
-//   render() {
-//     return (
-//       <div className="music" ref={this.ref} />
-//     );
-//   }
-// }
+const delays = {
+  facebook: 200,
+  twitter: 100,
+  instagram: 25,
+  spotify: 25,
+  youtube: 100,
+  soundcloud: 200
+}
 
 class Profile extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { show: false, inAnimation: false}
+    this.state = { animate: false }
 
-    this.handleToggle = this.handleToggle.bind(this)
-    this.registerCompletedAnimation = this.registerCompletedAnimation.bind(this)
+    this.profilePicRef = React.createRef();
+    this.facebookRef = React.createRef()
+    this.twitterRef = React.createRef()
+    this.instagramRef = React.createRef()
+    this.spotifyRef = React.createRef()
+    this.youtubeRef = React.createRef()
+    this.soundcloudRef = React.createRef()
+    this.musicRef = React.createRef()
+
+    this.setOrReset = this.setOrReset.bind(this)
+    this.animateElements = this.animateElements.bind(this)
   }
 
-  handleToggle() {
-    if (!this.state.inAnimation) {
-      this.setState(({ show }) => ({
-        show: !show,
-        inAnimation: true
-      }))
-    }
+  setOrReset() {
+    this.setState({
+      animate: !this.state.animate
+    });
   }
 
-  registerCompletedAnimation() {
-    if (this.state.inAnimation) {
-      this.setState({ inAnimation: false })
+  generateLinkElement = (link) =>
+    <div className="link" ref={this[`${link}Ref`]} key={link}>
+      <a href={hyperlinks[link]}>
+        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40">
+          <FontAwesomeIcon icon={['fab', link]} />
+        </svg>
+      </a>
+    </div>
+
+  animateElements = (animate) => {
+    // PIC ANIMATIONS
+
+    anime({
+      targets: this.profilePicRef.current,
+      easing: 'easeInOutQuart',
+      loop: false,
+      scale: animate ? 1.5 : 1,
+      delay: 0,
+      duration: 500
+    })
+
+    // LINKS ANIMATIONS
+
+    for (let i = 0; i < links.length; i++) {
+      const link = links[i]
+      anime({
+        easing: 'easeInOutQuart',
+        loop: false,
+        targets: this[`${link}Ref`].current,
+        translateX: animate ? translateX[link] : 0,
+        translateY: animate ? translateY[link] : 0,
+        delay: delays[link],
+        scale: animate ? 1.2 : 1,
+        opacity: 1,
+        duration: 500
+      });
     }
+
+    // MUSIC ANIMATIONS
+
+    anime.timeline()
+    .add({
+      targets: this.musicRef.current,
+      easing: 'easeInOutQuart',
+      loop: false,
+      translateX: animate ? -300 : 0,
+      translateY: animate ? 300 : 0,
+      borderRadius: '50%',
+      delay: 500,
+      duration: 500
+    })
+    .add({
+      targets: this.musicRef.current,
+      easing: 'easeInOutQuart',
+      loop: false,
+      borderRadius: animate ? '10%' : '50%',
+      delay: 0,
+      scale: animate ? 2 : 1,
+      duration: 500
+    })
+
   }
 
   render() {
-    const { show } = this.state
+    const { animate } = this.state
     return (
       <div className="profile">
-        <Transition in={show} timeout={1000}>
-          {status => (
-            <ProfileAnimation status={status} handleToggle={this.handleToggle} registerCompletedAnimation={this.registerCompletedAnimation} />
-          )}
+        <Transition
+          in={animate}
+          duration={2000}
+          timeout={2000}
+          onEnter={() => { this.animateElements(animate) }}
+          onExit={() => { this.animateElements(animate) }}>
+          <div>
+            <img className="profile-pic" ref={this.profilePicRef} src={imgPath} alt="Profile Pic" onClick={this.setOrReset} />
+            {links.map(link => this.generateLinkElement(link))}
+            <div className="music" ref={this.musicRef} />
+          </div>
         </Transition>
       </div>
     )
