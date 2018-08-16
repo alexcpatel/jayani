11955da172dfba97fd1ccfbacc21f88728779d1c
acpatel <acpatel@andrew.cmu.edu>
2018-08-16 18:51:00 -0400

    added ssl

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
-const http = require('http');
+const https = require('https');
+const fs = require('fs');
 const express = require('express');
+const path = require('path')
 
-// Express app setup
+const options = {
+  key: fs.readFileSync(path.join(__dirname, '/jayani_com.key')),
+  cert: fs.readFileSync(path.join(__dirname, '/jayani_com.csr')),
+  requestCert: false,
+  rejectUnauthorized: false
+};
+
 const app = express();
 
-app.use( express.static( `${__dirname}/build` ) );
+app.use(express.static(path.join(__dirname, '/build')));
 
-const path = require('path')
-app.get('*', (req, res)=>{
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/build/index.html'));
 })
 
-const server = http.createServer(app);
+const server = https.createServer(options, app);
 server.listen(80);
 server.on('listening', () => {
   console.log('Server is listening on port: 80');
